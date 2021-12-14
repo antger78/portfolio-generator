12541ca0c5b1dce22d56704f1aabf9975947ea4d
Anthony Geritano <anthony.geritano@aya.yale.edu>
2021-12-14 13:13:55 -0500

    added multiple projects to portfolio

--- app.js
+++ app.js
@@ -19,7 +19,10 @@ return inquirer.prompt([
   ])
 };
 
-const promptProject = () => {
+const promptProject = portfolioData => {
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  };
   console.log(`
     =================
     Add a New Project
@@ -59,12 +62,22 @@ const promptProject = () => {
         message: 'Would you like to enter another project?',
         default: false
       }
-    ])
+    ]).then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject){
+          return promptProject (portfolioData);
+        } else {
+          return portfolioData;
+        }
+    });
   };
   promptUser()
-  .then(answers => console.log(answers))
   .then (promptProject)
-  .then(projectAnswers => console.log(projectAnswers));
+  .then(portfolioData => {
+    console.log(portfolioData);
+  });
+  
+// .then(projectAnswers => console.log(projectAnswers));
 // const fs = require('fs');
 // const generatePage = require('./src/page-template');
 
